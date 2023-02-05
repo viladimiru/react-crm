f1b8c91c03a25f565058b9c3550637971b04230c
Vladimir <>
2023-02-06 00:31:12 +0300

    update

--- src/store/feedback/feedback.api.ts
+++ src/store/feedback/feedback.api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const feedbackApi = createApi({
-	reducerPath: 'users/api',
+	reducerPath: 'feedback/api',
 	baseQuery: fetchBaseQuery({ 
 		baseUrl: 'https://www.flamingo-house.top/api/feedback/',
 	}),
